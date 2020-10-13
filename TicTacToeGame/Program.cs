f3ada4bd49258b582a9d435e16909a3f8972cc4f
N1vas <ggj.nivas.18@gmail.com>
2020-10-13 09:50:39 +0530

    [N1vas] Add:UC1 TicTacToe Board is created

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game");
+            TicTacToeGame game = new TicTacToeGame();
+            game.createBoard();
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToeGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace TicTacToeGame
+{
+    class TicTacToeGame
+    {
+        public char[] createBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+    }
+}
